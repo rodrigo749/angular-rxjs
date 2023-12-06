4171bfc91029091ce06a6c652312f8fa794895f2
Rodrigo Neves Ottoboni <10837877+rodrigo749@users.noreply.github.com>
2023-12-06 10:10:40 -0300

    First Commit

---
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { EMPTY, catchError, debounceTime, distinctUntilChanged, filter, map, of, switchMap, tap, throwError } from 'rxjs';
+import { Item, LivrosResultado} from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
+import { LivroService } from 'src/app/service/livro.service';
+
+const PAUSA = 300;
+@Component({
+  selector: 'app-lista-livros',
+  templateUrl: './lista-livros.component.html',
+  styleUrls: ['./lista-livros.component.css']
+})
+export class ListaLivrosComponent {
+
+  campoBusca = new FormControl();
+  mensagemErro = '';
+  livrosResultado: LivrosResultado;
+  listaLivros: any
+
+  constructor(private service: LivroService) { }
+
+  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    tap(() => {
+      console.log('Fluxo inicial de dados');
+    }),
+    filter(
+      (valorDigitado) => valorDigitado.length >= 3
+    ),
+    switchMap(
+      (valorDigitado) => this.service.buscar(valorDigitado)
+    ),
+    map(resultado => this.livrosResultado = resultado),
+    map(resultado => resultado.items ?? []),
+    tap(console.log),
+    map(items => this.listaLivros = this.livrosResultadoParaLivros(items)),
+    catchError(erro =>
+      { console.log(erro);
+        return throwError(() =>
+        new Error(this.mensagemErro = `Ops, ocorreu um erro! Recarregue a aplicação!`));
+      })
+  );
+
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map(item => {
+      return new LivroVolumeInfo(item);
+    });
+  }
+
+
+}
+
+
+
